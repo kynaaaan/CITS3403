77fe7514d4a022023a9e1f99b4cc4bc9fa16604c
kynan <kynangallen14@gmail.com>
2026-05-15 11:58:11 +0800

    Protected routes - added login_required decorator to reviews.write, restaurants.add - In future mutating endpoints like review like/unlike, follow/unfollow, restaurant account owner editing endpoints all need to be protected. this will be done when they are implemented

--- app/reviews/routes.py
+++ app/reviews/routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 bp = Blueprint('reviews', __name__)
 
 
 @bp.route('/reviews/write')
+@login_required
 def write():
     return render_template('reviews/write_review.html')
